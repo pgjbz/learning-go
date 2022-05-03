a45fe535cd0859a4e04e6ba46fecf57b582b3979
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-05-03 20:21:48 -0300

    defer stack

--- defer/main.go
+++ defer/main.go
@@ -3,4 +3,13 @@ package main
 func main() {
 	defer print(", world!")
 	print("hello")
+
+	for i := 0; i < 10; i++ {
+		defer println(i)
+	}
+	/*
+	   defer is a stack call
+	*/
+
+	println("done")
 }
