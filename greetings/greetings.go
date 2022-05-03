08303fe50b1ed98766d91f6d1979eaf35c430971
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-05-03 11:10:06 -0300

    get random greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,27 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) { //use a tuple to return string and maybe an error
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomGreetings(), name)
 	return message, nil
+}
+
+func init() { //init function automatic execute
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomGreetings() string {
+	var formats []string = []string{
+		"Hi, %v. Welcome!",
+		"Hello, %v",
+		"Salve, %v",
+	}
+	return formats[rand.Intn(len(formats))]
 }
