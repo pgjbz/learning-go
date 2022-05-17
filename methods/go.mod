b734aee8f6b6c5b0c639ae537fc5d9c2da3f510a
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-05-17 15:59:03 -0300

    methods

---
+++ methods/go.mod
@@ -0,0 +1,3 @@
+module pgjbz.dev/methods
+
+go 1.18
---
+++ methods/main.go
@@ -0,0 +1,40 @@
+package main
+
+type Person struct {
+	name string
+	age  int8
+}
+
+func (p *Person) printName() {
+	println(p.name)
+}
+
+func (p *Person) printAge() {
+	println(p.age)
+}
+
+func (p *Person) changeName(name string) {
+	p.name = name
+}
+
+/*
+  methos are functions, and can be writer without the "receptor argument"
+  (receptor arugment is argument between func and name of function)
+
+  this function:
+
+  func printAge(person *Person) {
+    println(person.name)
+  }
+
+  has the same effect as printName with receptor argument
+
+*/
+
+func main() {
+	person := Person{"Paulo", 22}
+	person.printAge()
+	person.printName()
+	person.changeName("My name")
+	person.printName()
+}
