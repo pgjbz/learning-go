93306b0a8921f5f3d17d75c9b34ce814d02ee7ff
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-05-17 15:15:10 -0300

    functions is value

---
+++ function-is-value/go.mod
@@ -0,0 +1,3 @@
+module pgjbz.dev/function-is-value
+
+go 1.18
---
+++ function-is-value/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "math"
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func main() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	println(compute(hypot))
+	println(compute(math.Pow))
+}
